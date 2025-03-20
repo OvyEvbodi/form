d61ce57afd88eb5e1c00ec49a0b815bd2de0432c
Ovy Evbodi <evbodiovo@gmail.com>
2025-03-20 17:42:39 +0100

    feat: setup database connection

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,22 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  reactStrictMode: true,
+  swcMinify: true,
+
+  webpack: (config, { isServer }) => {
+    if (!isServer) {
+      config.resolve.fallback = {
+        fs: false,
+        dns: false,
+        net: false,
+        tls: false
+      };
+    }
+
+    return config;
+  },
+  
 };
 
 export default nextConfig;
--- utils/handleFormSubmit.ts
+++ utils/handleFormSubmit.ts
@@ -1,3 +1,28 @@
+'use server'
+import { Pool } from "pg";
+
+try {
+  const pool = new Pool({
+    user: process.env.USER,
+    host: process.env.HOST,
+    database: process.env.DB,
+    password: process.env.PASSWORD,
+    port: 5432
+  })
+  
+  const dbConnection = async() => {
+    const db = pool.connect((error, client, release) => {
+      if (error) {
+        console.log(error)
+      }
+      console.log("connected ooooo") // release connection
+    })
+  };
+  dbConnection()
+} catch (error) {
+  console.log(error)
+}
+
 
 export const handleFormSubmit = async (prevState: any, formData: FormData) => {
   const name = formData.get("name") as string;
