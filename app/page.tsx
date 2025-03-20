f6c2baa2a0f3d6b66d21582f2777c0615e4b3b45
Ovy Evbodi <evbodiovo@gmail.com>
2025-03-20 02:31:44 +0100

    feat: setup geolocation detection

--- app/page.tsx
+++ app/page.tsx
@@ -2,9 +2,9 @@ import PreForm from "@/components/PreForm";
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <h1 className="text-2xl font-bold">Kano State IEV Implementation Strategy</h1>
+    <div className="flex flex-col justify-center items-centermin-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+      <main className="flex flex-col justify-center items-center">
+        <h1 className="text-3xl font-extrabold">Kano State IEV Implementation Strategy</h1>
         <PreForm />
       </main>
     </div>
--- components/PreForm.tsx
+++ components/PreForm.tsx
@@ -8,22 +8,48 @@ const PreForm = () => {
   const [email, setEmail] = useState<string>('')
 
   const handleSubmit = async(event: React.FormEvent<HTMLFormElement>) => {
-    event.preventDefault()
+    try {
+      event.preventDefault()
     
-    // post email and geolocation details to endpoint
-    console.log({email, location: {lat: 5.34300, long: 5686.55}})
-    // window.location.href = 'https://forms.office.com/pages/responsepage.aspx?id=1ZaE-EQo30OpmOXT4XMGwrJ_u3WTTuJAkGoCkBzgQTRUMUtRWU9ZVE9CVEZENFgwMUtXWU1MUklRNC4u&route=shorturl';
+      const success = (position: any) => {
+        const latitude = position.coords.latitude;
+        const longitude = position.coords.longitude;
+
+        console.log({
+          message: "Geolocation successfully detected", 
+          email, 
+          latitude, 
+          longitude, 
+          position
+        })
+        // post email and geolocation details to endpoint
+
+        // redirect to main form
+        // window.location.href = 'https://forms.office.com/pages/responsepage.aspx?id=1ZaE-EQo30OpmOXT4XMGwrJ_u3WTTuJAkGoCkBzgQTRUMUtRWU9ZVE9CVEZENFgwMUtXWU1MUklRNC4u&route=shorturl';
+      }
+
+      if (!navigator.geolocation) {
+        console.log("Geolocation is not supported by your browser")
+      } else {
+        console.log("Getting location")
+        navigator.geolocation.getCurrentPosition(success); // add error callback
+      }
+    
+    } catch (error) {
+      // for dev
+      console.log(error)
+    }
+    
   }
   const handleEmail = (event: React.ChangeEvent<HTMLInputElement>) => {
     event.preventDefault()
     setEmail(event.target.value)
-    console.log(email)
   }
   
   return (
     <form onSubmit={ handleSubmit } className='p-6 flex flex-col gap-4'>
       <label htmlFor="email"></label>
-      <input type="email" id="email" name="email" required placeholder='Enter email' onChange={ handleEmail } className='rounded-md text-center p-4 border-1 border-y-gray-700'/>
+      <input type="email" id="email" name="email" required placeholder='Enter email' onChange={ handleEmail } className='rounded-md text-center p-4 px-8 md:px-14 border-1 border-y-gray-700'/>
       <button className='cursor-pointer bg-cyan-800 text-white p-4 rounded-md'>Start</button>
     </form>
   )
