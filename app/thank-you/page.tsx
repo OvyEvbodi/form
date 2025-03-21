fe4020ff74509aede958c6832706c0ecce821348
Ovy Evbodi <evbodiovo@gmail.com>
2025-03-21 04:36:36 +0100

    Feat: Add finally

--- app/thank-you/page.tsx
+++ app/thank-you/page.tsx
@@ -3,7 +3,11 @@ import checkIcon from "@/public/check_icon.png";
 const ThankYou = () => {
   return (
     <div className="flex justify-center items-center p-4 font-medium text-medium min-h-screen min-w-screen">
+      <div className="bg-cyan-700 flex flex-col justify-center items-center p-4">
+      <Image src={checkIcon} height={80} width={80} alt="check-icon"/>
       <div className="p-10 md:p-20 bg-cyan-700 text-white text-lg text-center">You have successfully completed the application process. <br /> We will reach out to you if you have passed the evaluation and are selected.</div>
+      </div>
+      
     </div>
   )
 };
