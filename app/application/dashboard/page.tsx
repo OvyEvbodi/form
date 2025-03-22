a227017e7138fac6a5fa2e0f8c03585446e29b5e
Ovy Evbodi <evbodiovo@gmail.com>
2025-03-22 12:13:43 +0100

    feat: add application dashboard page

---
+++ app/application/dashboard/page.tsx
@@ -0,0 +1,10 @@
+
+const Dashboard = () => {
+  return (
+    <div>
+      <iframe className="min-h-screen min-w-screen" title="Kano-IEV-Applicant-Assessment-Report" width="600" height="373.5" src="https://app.powerbi.com/view?r=eyJrIjoiMzM1ZjViZGQtNzBmOC00ZmRjLTg3YjktZjQwOTBkNzc5ZmE5IiwidCI6ImMzZDE1ZjkyLWIzZmQtNDFiNi1iNjcxLWJkN2Y5YmE1ZGUwNCJ9" allowFullScreen={true}></iframe>
+    </div>
+  )
+};
+
+export default Dashboard
