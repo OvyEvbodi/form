a90b9a2e6aec1f9591d99909ee6845d400866ead
Chee-Zaram Okeke <cheezaram.okeke@gmail.com>
2025-03-23 05:55:15 +0100

    Update page.tsx

--- app/application/dashboard/page.tsx
+++ app/application/dashboard/page.tsx
@@ -2,7 +2,7 @@
 const Dashboard = () => {
   return (
     <div>
-      <iframe className="min-h-screen min-w-screen" title="Kano-IEV-Applicant-Assessment-Report-Updated" width="600" height="373.5" src="https://app.powerbi.com/view?r=eyJrIjoiNGJkNWFjNmEtZmY4Yi00MmM3LTg5YmMtYjUyMjhkZjM4MjA3IiwidCI6ImMzZDE1ZjkyLWIzZmQtNDFiNi1iNjcxLWJkN2Y5YmE1ZGUwNCJ9&pageName=bc7df7cd08a048850d9a" allowFullScreen={true}></iframe>
+      <iframe className="min-h-screen min-w-screen" title="Kano-IEV-Applicant-Assessment-Report-Updated" width="600" height="373.5" src="https://app.powerbi.com/view?r=eyJrIjoiY2Y3ZTU1ZTAtZjE2MC00MTA2LWE4ZjctNzc1YmNhNjczY2FhIiwidCI6ImMzZDE1ZjkyLWIzZmQtNDFiNi1iNjcxLWJkN2Y5YmE1ZGUwNCJ9&pageName=bc7df7cd08a048850d9a" allowFullScreen={true}></iframe>
       
     </div>
   )
