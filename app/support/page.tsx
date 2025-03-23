d67c3c913e17e8259ac6d36f8bccabeb9b751ce1
Ovy Evbodi <evbodiovo@gmail.com>
2025-03-24 00:50:50 +0100

    chore: deactivate support page

--- app/support/page.tsx
+++ app/support/page.tsx
@@ -1,10 +1,22 @@
 import SupportForm from "@/components/SupportForm";
+import Link from "next/link";
 
 const SupportPage = () => {
+  const isDeactivated = true;
+
   return (
-    <div className="flex justify-center items-center min-w-screen min-h-screen">
-      <SupportForm />
+    <div>
+      <div className={ isDeactivated ? "hidden" : "flex justify-center items-center min-w-screen min-h-screen"}>
+        <SupportForm />
+      </div>
+      <div className={ !isDeactivated ? "hidden" : "flex justify-center items-center min-w-screen min-h-screen"}>
+        <p>
+          This page has been deactivated.
+        </p>
+        <Link href="/" className="block m-3 text-blue-800 underline hover:text-blue-600">Go back home</Link>
+      </div>
     </div>
+    
   )
 };
 
