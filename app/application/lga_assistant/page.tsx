071940285d9ee7be7bddce71759806fd1e9facae
Chee-Zaram Okeke <cheezaram.okeke@gmail.com>
2025-03-24 23:53:35 +0100

    Deactivate lga page

--- app/application/lga_assistant/page.tsx
+++ app/application/lga_assistant/page.tsx
@@ -4,7 +4,7 @@ import { clerk, ward } from "@/data/links";
 
 export default function LgaCoordinator() {
 
-  const isDeactivated = false;
+  const isDeactivated = true;
 
   return (
     <div className="bg_img_holder flex flex-col justify-center items-center min-h-screen p-6 -px-2 gap-6 lg:gap-16 lg:p-12 font-[family-name:var(--font-geist-sans)]">
@@ -15,10 +15,10 @@ export default function LgaCoordinator() {
         <div className="bg-cyan-700/60 flex flex-col justify-center items-center p-4">
         {/* <Image src={checkIcon} height={80} width={80} alt="check-icon"/> */}
         <div className="mb-6 p-10 md:p-20 bg-cyan-800/80 text-white text-base sm:text-lg text-center">
-          Thank you for your interest in the LGA Assistant role. Due to the number of submissions received,  application for this role is now closed. <br /> Kindly consider applying for: Ward Assistant or Data Clerk (females only)
+          Thank you for your interest in the LGA Assistant role. The application process is now closed and shortlisted applicants will be contacted shortly.
         </div>
-        <LinkRoute props={clerk} />
-        <LinkRoute props={ward} />
+          {/*<LinkRoute props={clerk} /> */}
+          {/*<LinkRoute props={ward} /> */}
         </div>
       </div>
     </div>
