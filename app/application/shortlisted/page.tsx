f25cb59fb2a091a654f7024e32358ed46332130f
Ovy Evbodi <evbodiovo@gmail.com>
2025-03-27 07:20:25 +0100

    fix: conditionally render shortlisted page

--- app/application/shortlisted/page.tsx
+++ app/application/shortlisted/page.tsx
@@ -6,20 +6,26 @@ const ShortlistedPage = () => {
 
   return (
     <div className="bg_img_holder flex flex-col justify-center items-center min-h-screen p-6 -px-2 gap-6 lg:gap-16 lg:p-12 font-[family-name:var(--font-geist-sans)]">
-      <div className={ isDeactivated ? "hidden" : "flex flex-col justify-center items-center" }>
-        <ShortlistedForm />
-      </div>
-      <div className={ !isDeactivated ? "hidden" : "flex justify-center items-center p-4 font-medium text-medium min-h-screen min-w-screen"}>
-        <div className="bg-cyan-700/60 flex flex-col justify-center items-center p-4">
-        {/* <Image src={checkIcon} height={80} width={80} alt="check-icon"/> */}
-        <div className="mb-6 p-10 md:p-20 bg-cyan-800/80 text-white text-base sm:text-lg text-center">
-        <p>
-          This page has been deactivated.
-        </p>
-        <Link href="/" className="block m-3 text-gray-400 underline hover:text-gray-200">Go back home</Link>
-        </div>
-        </div>
-      </div>
+      {
+        !isDeactivated &&
+          <div className={ isDeactivated ? "hidden" : "flex flex-col justify-center items-center" }>
+            <ShortlistedForm />
+          </div>
+      }
+      {
+        isDeactivated &&
+          <div className={ !isDeactivated ? "hidden" : "flex justify-center items-center p-4 font-medium text-medium min-h-screen min-w-screen"}>
+            <div className="bg-cyan-700/60 flex flex-col justify-center items-center p-4">
+              {/* <Image src={checkIcon} height={80} width={80} alt="check-icon"/> */}
+              <div className="mb-6 p-10 md:p-20 bg-cyan-800/80 text-white text-base sm:text-lg text-center">
+                <p>
+                  This page has been deactivated.
+                </p>
+                <Link href="/" className="block m-3 text-gray-400 underline hover:text-gray-200">Go back home</Link>
+              </div>
+            </div>
+          </div>
+      }
     </div>
   );
 };
