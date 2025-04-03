f05b6a60c9b2368967e1fa37f62ce706050cda6f
Ovy Evbodi <evbodiovo@gmail.com>
2025-04-03 20:34:48 +0100

    fix: Add error feedback for incorrect admin pin

--- app/application/admin/page.tsx
+++ app/application/admin/page.tsx
@@ -1,18 +1,27 @@
 import UserInfo from "@/components/UserInfo"
-import { getSession } from "@/lib"
+import { getSession, logout } from "@/lib"
 import { redirect } from "next/navigation"
 import { ErrorBoundary } from "react-error-boundary";
 
 const Admin = async () => {
   const session = await getSession();
   if (session === null) redirect("/application/auth")
+  
+  const handleSignOut = async () => {
+    "use server"
+    await logout()
+  };
 
   return (
     <div>
-      <h1>Admin page</h1>
+      <div className="flex justify-between p-2">
+      <h1 className="font-extrabold uppercase text-cyan-900 md:text-2xl">Admin page</h1>
+      <form action={handleSignOut}>
+        <button type="submit" className="lg:min-w-[180px] lg:min-h-[40px] cursor-pointer p-2 lg:py-[11px] px-[27px] text-white font-semibold capitalize bg-cyan-800 rounded-lg hover:bg-cyan-900 transition-all">logout</button>
+      </form>
+      </div>
       <ErrorBoundary fallback={<div className="text-center">Something's not right. Please refresh the page.</div>}>
         <UserInfo />
-        <pre>{JSON.stringify(session, null, 2)}</pre>
       </ErrorBoundary>
     </div>
   )
--- lib.ts
+++ lib.ts
@@ -3,6 +3,11 @@ import { cookies } from "next/headers";
 import { NextRequest, NextResponse } from "next/server";
 
 
+interface LoginResult {
+  success: boolean;
+  message?: string;
+}
+
 const secretKey = process.env.JWT_SECRET;
 const key = new TextEncoder().encode(secretKey);
 
@@ -10,7 +15,7 @@ export async function encrypt(payload: JWTPayload) {
   return await new SignJWT(payload)
     .setProtectedHeader({ alg: "HS256" })
     .setIssuedAt()
-    .setExpirationTime("10s")
+    .setExpirationTime("2h")
     .sign(key);
 }
 
@@ -21,23 +26,24 @@ export async function decrypt(input: string): Promise<any> {
   return payload;
 }
 
-export async function login(formData: FormData) {
+export async function login(formData: FormData): Promise<LoginResult> {
   
   const role = formData.get("role");
   const adminPin = process.env.ADMIN_PIN;
   const superAdminPin = process.env.SUPER_ADMIN_PIN;
   const payloadPin = formData.get("admin_pin");
-  if( role === "Admin" && adminPin !== payloadPin ) return "Incorrect pin for Admin user!"
-  else if( role === "Super Admin" && superAdminPin !== payloadPin ) return "Incorrect pin for Super Admin user!"
+  if( role === "Admin" && adminPin !== payloadPin ) return { success: false, message: "Incorrect pin for Admin user!" }
+  else if( role === "Super Admin" && superAdminPin !== payloadPin ) return { success: false, message: "Incorrect pin for Super Admin user!" }
 
   const user = {role};
 
   // Create the session // change expiration -------------------------
-  const expires = new Date(Date.now() + 10 * 1000);
+  const expires = new Date(Date.now() + 2 * 60 * 60 * 1000);
   const session = await encrypt({ user, expires });
 
   // Save the session in a cookie
   (await cookies()).set("session", session, { expires, httpOnly: true });
+  return { success: true, message: "Signed in successfully" };
 }
 export async function logout() {
   // Destroy the session
