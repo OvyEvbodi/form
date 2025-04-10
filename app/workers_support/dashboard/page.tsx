56520e1695db924779e5c59d5686ce3c72cb2e19
Chee-Zaram Okeke <cheezaram.okeke@gmail.com>
2025-04-10 13:49:48 +0000

    add workers_support dashboard

---
+++ app/workers_support/dashboard/page.tsx
@@ -0,0 +1,10 @@
+const Dashboard = () => {
+    return (
+      <div>
+        <iframe className="min-h-screen min-w-screen" title="Kano-IEV-Supervisor-Complaint-Dashboard" width="600" height="373.5" src="https://app.powerbi.com/view?r=eyJrIjoiYzY0MzE3YzAtYTVjZi00M2I0LWE3M2QtNTQ1ODJiOGZkZGFlIiwidCI6ImMzZDE1ZjkyLWIzZmQtNDFiNi1iNjcxLWJkN2Y5YmE1ZGUwNCJ9&pageName=bc7df7cd08a048850d9a" allowFullScreen={true}></iframe>
+      </div>
+    )
+  };
+  
+  export default Dashboard
+  
