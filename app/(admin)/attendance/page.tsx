e5f11a7d71829d5e989ef82dfb06ab5f73beabd3
Ovy Evbodi <evbodiovo@gmail.com>
2025-04-12 12:29:30 +0100

    chore: add navbar to attendance page

--- app/(admin)/attendance/page.tsx
+++ app/(admin)/attendance/page.tsx
@@ -4,6 +4,7 @@ import Link from "next/link";
 import { redirect } from "next/navigation"
 import { ErrorBoundary } from "react-error-boundary";
 import { PrismaClient } from "@prisma/client";
+import NavBar from "@/components/NavBar";
 
 const AttendanceAdmin = async () => {
   const session = await getSession();
@@ -42,9 +43,9 @@ const AttendanceAdmin = async () => {
 
   return (
     <div className="p-6">
+      <NavBar />
       <div className="min-h-[10vh] flex justify-between items-center p-2 md:pl-8 ">
         <h1 className="font-extrabold uppercase text-cyan-900 md:text-2xl">Attendance page</h1>
-        <Link href="/" className="text-cyan-800 hover:underline">Go Home</Link>
         <form action={handleSignOut}>
           <button type="submit" className="lg:min-w-[180px] lg:min-h-[40px] cursor-pointer p-2 lg:py-[11px] px-[27px] text-white font-semibold capitalize bg-cyan-800 rounded-lg hover:bg-cyan-900 transition-all">logout</button>
         </form>
