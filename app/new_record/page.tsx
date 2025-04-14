1c04bea31feef256fdbdc5fca152963849b22855
Ovy Evbodi <evbodiovo@gmail.com>
2025-04-14 15:54:46 +0100

    fix: change checkbox colour

--- app/new_record/page.tsx
+++ app/new_record/page.tsx
@@ -1,13 +1,15 @@
 import AddRecords from "@/components/AddRecords"
 import { getSession, logout } from "@/app/(admin)/attendance/auth/lib"
 import { redirect } from "next/navigation";
+import NavBar from "@/components/NavBar";
 
 const NewRecordpage = async () => {
 const session = await getSession();
   if (session === null) redirect("/attendance/auth")
     
   return (
-    <div className="p-4 md:p-12">
+    <div className=" flex flex-col justify-center items-center min-h-screen p-4">
+      <NavBar />
       <AddRecords {...session} />
     </div>
   )
